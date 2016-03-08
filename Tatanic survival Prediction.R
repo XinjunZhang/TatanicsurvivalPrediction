4cef76b72b3a71bf3c51fbba06ba7d16568ccfd0
XinjunZhang <visionretina@gmail.com>
2016-03-07 23:10:33 -0500

    first commit

---
+++ Tatanic survival Prediction.R
@@ -0,0 +1,65 @@
+data<-read.csv("Tatanic/train.csv",na.strings=c("","NA"))
+
+library(dplyr)
+library(caret)
+library(rpart)
+library(stringr)
+
+
+data$Parch[data$Parch !=0]<-1
+summar(data)
+
+col_names<-c("Survived","Pclass","Sex","Age","Embarked","Parch")
+data[,col_names] <- lapply(data[,col_names], factor)
+data$Age<-as.numeric(data$Age)
+data$SibSp<-as.numeric(data$SibSp)
+summary(data)
+
+data<-data[,-c(1,4,9,11)]
+data<-na.omit(data)
+summary(data)
+
+set.seed(96)
+inTrain <- createDataPartition(data$Survived, p = .8,
+                               list = FALSE,
+                               times = 1)
+training <- data[inTrain,]
+test <- data[-inTrain,]
+
+rtGrid <- expand.grid(cp=seq(0.001, 0.2, by = 0.001))
+# alpha is between 0 and 1: 0 (ridge), 1 (lasso).
+
+########################################## training model
+ctrl <- trainControl(method = "cv", number = 10,
+                     verboseIter = T)
+set.seed(1)
+rtTune <- train(Survived~ ., data = training,   
+                method = "rpart", 
+                tuneGrid = rtGrid,
+                metric='Kappa',
+                trControl = ctrl)
+
+rtTune
+plot(rtTune)
+rtTune$bestTune
+
+fancyRpartPlot(rtTune$finalModel)
+
+pr_ct <- predict(rtTune, newdata = test)
+
+
+ct_CM <- confusionMatrix(pr_ct, test$Survived, positive = "1")
+ct_CM
+
+###ROC Curve
+library(pROC)
+probsTrain<-predict(rtTune,training,type = "prob")
+rocCurve<-roc(response=training$Survived,predictor=probsTrain[,"1"],
+              levels = levels(as.factor(training$Survived)))
+
+plot(rocCurve,print.thres="best")
+
+names(rocCurve)
+rocCurve$thresholds
+rocCurve$sensitivities
+
